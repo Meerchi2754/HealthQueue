be488d533d015c2c55a2ccf9fd6ce9aa65511b87
Meerchi2754 <meetrajparmar556@gmail.com>
2026-03-07 20:14:10 +0530

    Authentication Routes. Successfull Database connection Establish. Added routes for login and register

---
+++ app/api/(auth)/login/route.ts
@@ -0,0 +1,50 @@
+import prisma from "@/lib/prisma";
+import bcrypt from "bcryptjs";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.json();
+    const { email, password } = body;
+
+    if (!email || !password) {
+      return NextResponse.json(
+        { message: "Email and Password required" },
+        { status: 400 },
+      );
+    }
+
+    const response = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!response) {
+      return NextResponse.json(
+        {
+          message: "User Not Found",
+        },
+        {
+          status: 401,
+        },
+      );
+    }
+    const isMatch = await bcrypt.compare(password, response.password);
+
+    if (!isMatch) {
+      return NextResponse.json(
+        { message: "Password Mismatch" },
+        { status: 401 },
+      );
+    }
+    const { password: _, ...rest } = response;
+
+    return NextResponse.json(
+      { message: "Login Successfull", data: rest },
+      { status: 201 },
+    );
+  } catch (error) {
+    return NextResponse.json({ error: error }, { status: 400 });
+  }
+}
---
+++ app/api/(auth)/register/route.ts
@@ -0,0 +1,45 @@
+import prisma from "@/lib/prisma";
+import bcrypt from "bcryptjs";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    const { email, name, password } = body;
+    
+    if (!email || !password) {
+      return NextResponse.json(
+        { message: "Email and Password required" },
+        { status: 400 },
+      );
+    }
+    const existingUser = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (existingUser) {
+      return NextResponse.json(
+        { message: "User already exists" },
+        { status: 400 },
+      );
+    }
+
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    const response = await prisma.user.create({
+      data: {
+        name: name,
+        email: email,
+        password: hashedPassword,
+      },
+      omit: { password },
+    });
+    return NextResponse.json(response, { status: 201 });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Something went wrong" },
+      { status: 500 },
+    );
+  }
+}
